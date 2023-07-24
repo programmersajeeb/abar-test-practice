ee346151c2f2e54aaaaeaecf3451d1718c130f92
Shajeeb <liveclasswd@gmail.com>
2023-07-24 18:19:21 +0600

    [DONE] express, mongoose, ts node dev

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+export default app;
+
+// mongodb atlas pass 3yluxwqKOI9iRZAZ
