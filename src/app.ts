b0a630212a736adacca4a55e4549917c10b82d7d
Shajeeb <liveclasswd@gmail.com>
2023-07-27 14:31:26 +0600

    add husky lint-staged

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 const app: Application = express();
-const port = 3000;
 
 // middleware
 app.use(cors());
@@ -10,8 +9,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
 export default app;
