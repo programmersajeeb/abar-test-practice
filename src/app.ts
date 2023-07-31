e579de1acad1f8ccdd563fa7d23cb9b86b24798c
Shajeeb <liveclasswd@gmail.com>
2023-07-31 14:57:21 +0600

    [DONE] add basic logger setup

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,19 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-// import userService from './app/modules/users/user.service';
 import router from './app/modules/users/user.route';
 const app: Application = express();
 
 // middleware
 app.use(cors());
 
-// api routes
-app.use('/api/v1/users', router);
-
 // parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// api routes
+app.use('/api/v1/users', router);
+
 app.get('/', async (req: Request, res: Response) => {
-  // await userService.createUser({
-  //   id: '23523',
-  //   role: 'student',
-  //   password: ';laskdjf',
-  // });
   res.send('Hello World!');
 });
 
