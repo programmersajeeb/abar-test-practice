d8aa0a43e36b258c2b35953f8a0c73e365af0a03
Shajeeb <liveclasswd@gmail.com>
2023-09-30 16:10:33 +0600

    [DONE] add not found route

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import globalErrorHandlers from './app/middlewares/globalErrorHandlers';
 import routes from './app/routes';
+import httpStatus from 'http-status';
 const app: Application = express();
 
 // middleware
@@ -21,5 +22,18 @@ app.get('/', (req: Request, res: Response) => {
 
 // global error handler
 app.use(globalErrorHandlers);
+
+// not found
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Api no route found',
+    errorMessage: {
+      path: req.originalUrl,
+      message: 'Not found the desired route',
+    },
+  });
+  next();
+});
 
 export default app;
